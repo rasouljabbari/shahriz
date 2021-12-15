9abef5af61e7e46503693365d2a7784edd221484
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2021-12-16 00:33:53 +0330

    schedule-concert page start

--- assets/js/index.js
+++ assets/js/index.js
@@ -119,3 +119,27 @@ let colleaguesSwiper = new Swiper(".colleaguesSwiper", {
     },
   },
 });
+
+let concertSwiper = new Swiper(".concertSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 35,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+});
