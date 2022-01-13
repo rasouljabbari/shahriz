11273444da7dc52630c36f9f4cc5c0cd8c85fb9f
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2022-01-13 22:57:39 +0330

    set edits

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,5 +1,7 @@
 // leaflet map
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([document.getElementById('map').getAttribute('lat'), document.getElementById('map').getAttribute('lng')], 13);
+
+console.log()
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=sk.eyJ1IjoicmpkZXZlbG9wZXIiLCJhIjoiY2t4OWhiZXVzMXJzcTJ3bzViZjJ6Z2Y1cCJ9.nEmZAc_kjXBwRMy5ade0UA', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -10,4 +12,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: 'your.mapbox.access.token'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([document.getElementById('map').getAttribute('lat'), document.getElementById('map').getAttribute('lng')]).addTo(map);
